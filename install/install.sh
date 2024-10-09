bcb30a484c0bd926bec1cb68a07711a7c221bd01
GitAlasGo <wjGQiwhUsbUsi9@gmail.com>
2024-10-09 05:13:35 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,13 @@
 #! /data/data/com.termux/files/usr/bin/bash
 cd ~
 pkg install wget -y
-rm -rf ~/Alabanta_Downloader
-rm -rf $PREFIX/tmp/install.sh
 mkdir -p ~/Alabanta_Downloader
+rm -rf ~/Alabanta_Downloader/*
+rm -rf $PREFIX/tmp/install.sh
 cd ~/Alabanta_Downloader
 wget https://raw.githubusercontent.com/GitAlasGo/Alabanta_Downloader/refs/heads/main/Script/{Alabanta_Downloader.sh,Alabanta_Downloader_Settings}
 chmod +x ~/Alabanta_Downloader/Alabanta_Downloader.sh
 mkdir -p ~/bin
 # If you already have a termux-url-opener, i'm just gonna move it so i can put mine
-mv $HOME/bin/termux-url-opener $HOME
+mv --backup=t $HOME/bin/termux-url-opener $HOME
 ln -s ~/Alabanta_Downloader/Alabanta_Downloader.sh $HOME/bin/termux-url-opener
