daf2f7535fe2e3f2124f576d4100fe5cf6c417ba
GitAlasGo <wjGQiwhUsbUsi9@gmail.com>
2024-10-09 17:41:21 +0300

    Update Axelly_Downloader.sh

--- Script/Axelly_Downloader.sh
+++ Script/Axelly_Downloader.sh
@@ -4,7 +4,8 @@ echo "threads set to $threads"
 echo
 read -e -p "file name: " -i "$(sed -r 's#.*/##' <<< $1)" name
 read -p 'thread: ' thread
+read -e -p 'path: ' -i "$path"  n_path
 
 [[ $thread =~ ^[0-9]+$ ]] && threads=$thread || echo "Using Default threads"
 echo "Threads set to $threads"
-axel -a -n $threads -o "$path$name" $1
+axel -a -n $threads -o "$n_path/$name" $1
