1f3560e6668af8df570812a50157c1ac3265c209
GitAlasGo <wjGQiwhUsbUsi9@gmail.com>
2024-10-09 04:28:29 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 #! /data/data/com.termux/files/usr/bin/bash
 cd ~
-wget https://raw.githubusercontent.com/GitAlasGo/Alabanta_Downloader/refs/heads/main/Script/{Alabanta_Downloader.sh,Alabanta_Downloader_Settings}
+axel -n 10 https://raw.githubusercontent.com/GitAlasGo/Alabanta_Downloader/refs/heads/main/Script/{Alabanta_Downloader.sh,Alabanta_Downloader_Settings}
 chmod +x ~/Alabanta_Downloader.sh
 mkdir -p ~/bin
 ln -s ~/Alabanta_Downloader.sh $HOME/bin/termux-url-opener
