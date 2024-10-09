44464bc998460c6e58bc1b572ae23616d36af7d0
GitAlasGo <wjGQiwhUsbUsi9@gmail.com>
2024-10-09 16:07:13 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,6 @@
 #! /data/data/com.termux/files/usr/bin/bash
-cd ~
-pkg install wget -y
-mkdir -p ~/Alabanta_Downloader
-rm -rf ~/Alabanta_Downloader/*
-rm -rf $PREFIX/tmp/install.sh
-cd ~/Alabanta_Downloader
-wget https://raw.githubusercontent.com/GitAlasGo/Axelly_Downloader/refs/heads/main/Script/{Alabanta_Downloader.sh,Alabanta_Downloader_Settings}
-chmod +x ~/Alabanta_Downloader/Alabanta_Downloader.sh
+pkg install axel -y
 mkdir -p ~/bin
 # If you already have a termux-url-opener, i'm just gonna move it so i can put mine
 mv --backup=t $HOME/bin/termux-url-opener $HOME
-ln -s ~/Alabanta_Downloader/Alabanta_Downloader.sh $HOME/bin/termux-url-opener
+ln -s ~/Axelly_Downloader/Script/Axelly_Downloader.sh $HOME/bin/termux-url-opener
